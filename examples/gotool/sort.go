e6affe9d2c90a4223a1a458ec72c53b0b902455f
Daniil Porokhnin <dkharmsd@gmail.com>
2026-01-19 19:45:20 +0300

    chore: tmp commit

--- examples/gotool/sort.go
+++ examples/gotool/sort.go
@@ -1,7 +1,10 @@
 package gotool
 
+import "time"
+
 func BubbleSort(s []int) []int {
 	for i := 0; i < len(s); i++ {
+		time.Sleep(time.Millisecond)
 		for j := 1; j < len(s); j++ {
 			if s[j-1] > s[j] {
 				s[j-1], s[j] = s[j], s[j-1]
